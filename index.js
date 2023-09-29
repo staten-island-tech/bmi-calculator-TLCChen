fb65b76d8244deef4bbf9b57aaa51d58c0f8102f
TLCChen <139132998+TLCChen@users.noreply.github.com>
2023-09-29 13:18:21 -0400

    stuff

---
+++ index.js
@@ -0,0 +1,14 @@
+let variable; //variable declaration
+variable = "Variable"; //variable definition
+let number = 7; //number data type
+let string = "Hello"; //string data type
+let truthy = true; //boolean
+let falsey = false; //boolean
+let bigString = "Hello I'm a bigger string"; //camel case
+
+var variable2 = "var"; //we dont do this
+let variable3; //we can define later OR REDEFINE
+variable = "nope";
+console.log(variable);
+const constant = "constant"; //cannot redefine
+//constant = "ibnosaaoibnqwdnp";
