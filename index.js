f536528c5a5d3a1d7708c8a38a2c97b1c561f59d
TLCChen <139132998+TLCChen@users.noreply.github.com>
2023-10-04 22:42:41 -0400

    10-4-23:home

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const height = Number(prompt("Please Enter Your Height in centimeters "));
 const weight = Number(prompt("Please Enter Your Weight in Kilograms: "));
 
-function bmi1() {
+function cal() {
   const meter = height / 100;
   const metsq = meter * meter;
   const bmi = weight / metsq;
-  console.log("You are", bmi, "kg/meters squared.");
   return bmi;
 }
 
-function evaluation(bmi1) {
-  bmi = bmi1;
-  if (bmi >= 30) {
-    console.log("You are obesit.");
-  } else if (bmi >= 25) {
+function text(bmi){
+  const text1 = "Your BMI is"
+  const text2 = "."
+  const together = `${text1} ${bmi}` + text2
+  console.log(together)
+}
+
+function evaluation(x) {
+  if (x >= 30) {
+    console.log("You are obese.");
+  } else if (x >= 25) {
     console.log("You are overweight.");
-  } else if (bmi >= 18.5) {
+  } else if (x >= 18.5) {
     console.log("You are normal weight.");
   } else {
     console.log("You are underweight.");
   }
 }
 
-bmi1();
-evaluation();
+text(cal());
+evaluation(cal());
